f0a2e98b276e5519f50d2398be87babe0005d4dc
Apoloni Pachulia <a.pachulia@hotmail.com>
2015-02-19 13:39:33 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?PHP
+
+require_once 'sdk/facebook.php';
+require_once 'connect.php';
+
+define("APPLICATION_ID","");
+define("APPLICATION_SECRET","");
+define("ACCESSTOKEN",""); //Generated Access Token
+
+
+$Facebook = new Facebook(array(
+  'appId'  => APPLICATION_ID,
+  'secret' => APPLICATION_SECRET
+));
+
+
+
+$Facebook->setFileUploadSupport(true);
+$Facebook->setAccessToken(ACCESSTOKEN);
+
+$PageID = ""; //Your Facebook Page Numeric ID
+
+
+$getPost=mysql_query("SELECT postID, Message, Picture FROM post WHERE postID=1 LIMIT 1");
+
+
+
+
+
+$PostID = mysql_result($getPost, 0, "PostID");
+$Message = mysql_result($getPost, 0, "Message");
+$Picture = mysql_result($getPost, 0, "Picture");
+$link="http://www.example.com";
+ 
+
+$Args = array('message' => $Message,'link'=>$link,'picture'=>$Picture,"access_token" => ACCESSTOKEN);
+
+
+try {
+
+$result = $Facebook->api("/".$PageID."/feed","post",$Args);
+
+} catch (Exception $e) {
+
+  echo 'Caught exception: ',  $e->getMessage(), "\n";
+
+ }
+
+		
+
+
+
+
+
+
+?>
